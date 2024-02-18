0b8ebfda725274aa64040d02f9c18bafaceae2af
Edwin Mburu Ng'ethe <43747077+justMburu@users.noreply.github.com>
2024-02-18 11:50:06 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Today we will be creating a rock paper scissors game" << endl;
+
+    void Player (){
+        int playerChoice;
+
+    }
+    void ComPlayer (){
+
+    }
+    void determineWinner (){
+
+    }
+
+    return 0;
+}
